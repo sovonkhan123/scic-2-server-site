6c34585e4a57ff85491dd01272e7b975b288114c
Azmiruzzaman khan sovon <sovonkhan77@gmail.com>
2024-02-22 09:35:47 +0600

    get method are added

--- index.js
+++ index.js
@@ -27,6 +27,13 @@ async function run() {
 
     const userCollection = client.db('task-user').collection('user');
 
+    app.get('/contest/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) }
+        const result = await userCollection.findOne(query);
+        res.send(result);
+      })
+
     app.post('/contest', async (req, res) => {
         const item = req.body;
         const result = await userCollection.insertOne(item);
