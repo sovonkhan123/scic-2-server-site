164ce58cd3fb0b7879887c82e9ab797f08ed1321
Azmiruzzaman khan sovon <sovonkhan77@gmail.com>
2024-02-22 09:00:19 +0600

    middleware are added

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+
+const port = process.env.PORT || 5000;
+
+// middlewire
+app.use(cors());
+app.use(express.json());
